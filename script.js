405db79a6601e0935b5930a38e96b6949539ea0a
Olamilekan2009 <abdulmumeenannujoom@gmail.com>
2024-08-18 11:53:40 -0700

    Edit script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+alert("Welcome To The Rock Paper Scissors Game !!!!! \n Click The Button Below To Start The Round One Of The Game");
+function ropasc() {
+    var firsthuman=prompt("What do you want to play: \n Rock or Paper or Scissors");
+}
+ropasc()
